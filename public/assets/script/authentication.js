8e9528eb1f0ba7c177c66d83b1bd43553ee65d49
Marcos Lee <marcoslee@Marcoss-MacBook-Pro.local>
2019-05-29 00:00:59 -0700

    add new login page, initiate firebase auth

---
+++ public/assets/script/authentication.js
@@ -0,0 +1,21 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAqBWzkmfuATx4S77dN03CUMdUTpeiJl6M",
+    authDomain: "pwa-lunch-god.firebaseapp.com",
+    databaseURL: "https://pwa-lunch-god.firebaseio.com",
+    projectId: "pwa-lunch-god",
+    storageBucket: "pwa-lunch-god.appspot.com",
+    messagingSenderId: "1028719724419",
+    appId: "1:1028719724419:web:c8305c7e544862a9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  function signInWithGoogle() {
+    var googleAuthProvider = new firebase.auth.GoogleAuthProvider;
+    firebase.auth().signInWithPopup(googleAuthProvider).then(function(data){
+        console.log(data)
+    }).catch(function(err){
+        console.log(err)
+    })
+  }
